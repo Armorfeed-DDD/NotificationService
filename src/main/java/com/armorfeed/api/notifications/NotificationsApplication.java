ec762b379701b1ceca710a32fae4b282d57de2c7
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-05-22 23:53:44 -0500

    feature: added token interceptor for token validation

--- src/main/java/com/armorfeed/api/notifications/NotificationsApplication.java
+++ src/main/java/com/armorfeed/api/notifications/NotificationsApplication.java
@@ -2,6 +2,7 @@ package com.armorfeed.api.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 @SecurityScheme(name = "armorddd", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition
 @SpringBootApplication
+@EnableFeignClients
 public class NotificationsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/armorfeed/api/notifications/providers/feignclients/UsersServiceFeignClient.java
+++ src/main/java/com/armorfeed/api/notifications/providers/feignclients/UsersServiceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.armorfeed.api.notifications.providers.feignclients.dto.AuthTokenResponse;
 
-@FeignClient(value = "users-service", url = "http://localhost:8080")
+@FeignClient(value = "users-service", url = "http://localhost:6719")
 public interface UsersServiceFeignClient {
     @GetMapping("/api/v1/users/auth/validate-token/{token}")
     public AuthTokenResponse validateToken(@PathVariable("token") String token);
